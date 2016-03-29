3c1531b6a264bddc8193156e6d1455cbf9113fe0
akashche <akashche@redhat.com>
2016-03-29 10:13:34 -0700

    top level run script

---
+++ run.bat
@@ -0,0 +1,20 @@
+@echo on
+
+rem shortcuts from script directory
+set BAD_SLASH_SCRIPT_DIR=%~dp0
+set SCRIPT_DIR=%BAD_SLASH_SCRIPT_DIR:\=/%
+set OJDKBUILD_DIR=%SCRIPT_DIR%
+
+call "%OJDKBUILD_DIR%/resources/scripts/set-compile-env.bat"
+if 0 neq %ERRORLEVEL% exit /b 1
+
+rmdir /s /q build
+if exist build exit /b 1
+mkdir build || exit /b 1
+pushd build || exit /b 1
+
+cmake ../src/java-1.8.0-openjdk || exit /b 1
+nmake openjdk || exit /b 1
+
+popd || exit /b 1
+echo OJDKBUILD_FINISH_SUCCESS
