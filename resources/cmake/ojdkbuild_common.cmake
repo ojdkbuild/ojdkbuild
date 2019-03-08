27c0fc1d77d53e22c20dd666fcfcc4fff6cffc5d
akashche <akashche@redhat.com>
2019-03-08 07:20:21 -0800

    11 and 12 minor cleanup, contrib utils added

--- resources/cmake/ojdkbuild_common.cmake
+++ resources/cmake/ojdkbuild_common.cmake
@@ -19,7 +19,7 @@ if ( NOT DEFINED OJDKBUILD_COMMON )
     OPTION ( BUILD_SHARED_LIBS "Build shared libraries" OFF )
     OPTION ( CMAKE_INCLUDE_DIRECTORIES_BEFORE "Prefer local libs over system ones" ON )
     set_property ( GLOBAL PROPERTY USE_FOLDERS ON )
-    
+
     # output directories
     set ( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin CACHE INTERNAL "" )
     set ( CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_BINARY_DIR}/bin CACHE INTERNAL "" )
@@ -30,27 +30,28 @@ if ( NOT DEFINED OJDKBUILD_COMMON )
     set ( CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin CACHE INTERNAL "" )
     set ( CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG ${CMAKE_BINARY_DIR}/bin CACHE INTERNAL "" )
     set ( CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE ${CMAKE_BINARY_DIR}/bin CACHE INTERNAL "" )    
-    
+
     # compiler flags
     if ( NOT DEFINED CMAKE_BUILD_TYPE )
         set ( CMAKE_BUILD_TYPE "Release" CACHE STRING "Build type" )
     elseif ( NOT  CMAKE_BUILD_TYPE MATCHES "Release|Debug" )
         message ( FATAL_ERROR "Unsupported build type: [${CMAKE_BUILD_TYPE}]" )
     endif ( )    
-    if ( NOT WIN32 )
-        message ( FATAL_ERROR "Unsupported platform" )
+    if ( WIN32 )
+        set ( CMAKE_C_FLAGS "/EHsc /D _LIB /D _CRT_SECURE_NO_WARNINGS /D WIN32_LEAN_AND_MEAN /D_WIN32_WINNT=0x0601" CACHE INTERNAL "" )
+        set ( CMAKE_C_FLAGS_DEBUG "/D _DEBUG /MT /Zi /Ob0 /Od /RTC1" CACHE INTERNAL "" )
+        set ( CMAKE_C_FLAGS_RELEASE "/D NDEBUG /MT /Zi /Ox /Gy /GT /GR-" CACHE INTERNAL "" )
+        set ( CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE INTERNAL "" )
+        set ( CMAKE_CXX_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG}" CACHE INTERNAL "" )
+        set ( CMAKE_CXX_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE}" CACHE INTERNAL "" )
+        set ( CMAKE_EXE_LINKER_FLAGS_DEBUG "/DEBUG /MAP" CACHE INTERNAL "" )
+        set ( CMAKE_EXE_LINKER_FLAGS_RELEASE "/DEBUG /OPT:REF /OPT:ICF /MAP" CACHE INTERNAL "" )
+        set ( CMAKE_SHARED_LINKER_FLAGS_DEBUG "/DEBUG /MAP" CACHE INTERNAL "" )
+        set ( CMAKE_SHARED_LINKER_FLAGS_RELEASE "/DEBUG /OPT:REF /OPT:ICF /MAP" CACHE INTERNAL "" )
+    else ( )
+        message ( WARNING "Running on non-Windows platform" )
     endif ( )
-    set ( CMAKE_C_FLAGS "/EHsc /D _LIB /D _CRT_SECURE_NO_WARNINGS /D WIN32_LEAN_AND_MEAN /D_WIN32_WINNT=0x0601" CACHE INTERNAL "" )
-    set ( CMAKE_C_FLAGS_DEBUG "/D _DEBUG /MT /Zi /Ob0 /Od /RTC1" CACHE INTERNAL "" )
-    set ( CMAKE_C_FLAGS_RELEASE "/D NDEBUG /MT /Zi /Ox /Gy /GT /GR-" CACHE INTERNAL "" )
-    set ( CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE INTERNAL "" )
-    set ( CMAKE_CXX_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG}" CACHE INTERNAL "" )
-    set ( CMAKE_CXX_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE}" CACHE INTERNAL "" )
-    set ( CMAKE_EXE_LINKER_FLAGS_DEBUG "/DEBUG /MAP" CACHE INTERNAL "" )
-    set ( CMAKE_EXE_LINKER_FLAGS_RELEASE "/DEBUG /OPT:REF /OPT:ICF /MAP" CACHE INTERNAL "" )
-    set ( CMAKE_SHARED_LINKER_FLAGS_DEBUG "/DEBUG /MAP" CACHE INTERNAL "" )
-    set ( CMAKE_SHARED_LINKER_FLAGS_RELEASE "/DEBUG /OPT:REF /OPT:ICF /MAP" CACHE INTERNAL "" )
-    
+
     # subdirectory macro
     macro ( ojdkbuild_add_subdirectory _project_path )
         string ( REGEX REPLACE "^.*/" "" _target_name ${_project_path} )
@@ -85,6 +86,32 @@ if ( NOT DEFINED OJDKBUILD_COMMON )
         set ( ENV{PKG_CONFIG_PATH} "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/pkgconfig${PATHENV_SEPARATOR}$ENV{PKG_CONFIG_PATH}" )
         pkg_check_modules ( ${_out_var_name} ${_modifier} ${${_modules_list_var_name}} )
         set ( ENV{PKG_CONFIG_PATH} ${_pkgconfig_path} )
+    endmacro ( )
+
+    # tests macro
+    macro ( ojdkbuild_enable_testing _includes_var_name _libs_var_name _opts_var_name )
+        set ( ${PROJECT_NAME}_CXX_EXTENSION cpp CACHE INTERNAL "" )
+        set ( ${PROJECT_NAME}_TEST_POSTFIX _test CACHE INTERNAL "" )
+        enable_testing ( )
+        file ( GLOB ${PROJECT_NAME}_TEST_SRC
+                RELATIVE ${CMAKE_CURRENT_LIST_DIR}
+                ${CMAKE_CURRENT_LIST_DIR}/*${${PROJECT_NAME}_TEST_POSTFIX}.${${PROJECT_NAME}_CXX_EXTENSION} )
+        foreach ( _testsrc ${${PROJECT_NAME}_TEST_SRC} )
+            string ( REPLACE .${${PROJECT_NAME}_CXX_EXTENSION} "" _testname ${_testsrc} )
+            message ( STATUS "Adding test: [${_testname}]" )
+            add_executable ( ${_testname} ${CMAKE_CURRENT_LIST_DIR}/${_testsrc} )
+            target_include_directories ( ${_testname} BEFORE PRIVATE ${${_includes_var_name}} )
+            target_link_libraries ( ${_testname} ${${_libs_var_name}} )
+            target_compile_options ( ${_testname} PRIVATE ${${_opts_var_name}} )
+            set_target_properties ( ${_testname} PROPERTIES FOLDER "test" )
+            if ( DEFINED CMAKE_MEMORYCHECK_COMMAND )
+                add_test ( ${_testname}
+                        ${CMAKE_MEMORYCHECK_COMMAND} ${CMAKE_MEMORYCHECK_COMMAND_OPTIONS}
+                        ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${_testname} )
+            else ( )
+                add_test ( ${_testname} ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${_testname} )
+            endif ( )
+        endforeach ( )
     endmacro ( )
 
     # lookaside dirs
